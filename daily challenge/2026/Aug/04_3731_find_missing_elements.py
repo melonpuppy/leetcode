e2ea4f5bae657cea047d1af5b931bdc798253703
melo <m310pup@gmail.com>
2026-08-10 11:15:36 +0800

    Add solution for 3731

---
+++ daily challenge/2026/Aug/04_3731_find_missing_elements.py
@@ -0,0 +1,5 @@
+# One-line solution:
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        st = set(nums)
+        return [i for i in range(min(nums) + 1, max(nums)) if i not in st]
