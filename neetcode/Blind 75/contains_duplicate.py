c4f15847d4f60136368f8537a630f935f765fe74
melo <m310pup@gmail.com>
2026-08-18 15:10:29 +0800

    Add other solutions of Contains Duplicate

--- neetcode/Blind 75/contains_duplicate.py
+++ neetcode/Blind 75/contains_duplicate.py
@@ -1,3 +1,23 @@
+# Sorting
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+
+#Hash Set
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
+
 # Hash Set Length
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
